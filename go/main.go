0678544d65e7c0cf1b3fe6a0fc894d1509919a62
karamaru-alpha <mrnk3078@gmail.com>
2022-07-17 07:37:47 +0900

    a

--- go/main.go
+++ go/main.go
@@ -114,6 +114,7 @@ func (h *handlers) Initialize(c echo.Context) error {
 		"1_schema.sql",
 		"2_init.sql",
 		"3_sample.sql",
+		"4.sql",
 	}
 	for _, file := range files {
 		data, err := os.ReadFile(SQLDirectory + file)
